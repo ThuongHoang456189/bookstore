84f75e51fa0f128cab0f6c80b1c389c1d0f1a617
ntnduy <ntnduy2905@gmail.com>
2019-07-06 14:43:34 +0700

    update souce

--- bookstore/src/main/java/pq/jdev/b001/bookstore/publishers/repository/PublisherRepository.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/publishers/repository/PublisherRepository.java
@@ -1,13 +1,19 @@
 package pq.jdev.b001.bookstore.publishers.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
-import pq.jdev.b001.bookstore.models.Publishers;
+import pq.jdev.b001.bookstore.Category.model.Category;
+import pq.jdev.b001.bookstore.publisher.models.Publishers;
+
+import java.util.List;
 
 @Repository
 public interface PublisherRepository extends CrudRepository<Publishers, Long> {
-	List<Publishers> findByCreateName(String createName);
-	
+	public List<Publishers> findAll();
+	public void deletePublisher(int publisherId);
+	public Publishers find(int publisherID);
 }
