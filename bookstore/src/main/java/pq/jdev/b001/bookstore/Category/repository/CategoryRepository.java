f9bceaa697933e536932c6f5f750896afdab5a23
ThuongHoang645189 <ngocanh654189@gmail.com>
2019-07-14 15:00:01 +0700

    UpdateBookManagement

--- bookstore/src/main/java/pq/jdev/b001/bookstore/Category/repository/CategoryRepository.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/Category/repository/CategoryRepository.java
@@ -1,15 +1,13 @@
 package pq.jdev.b001.bookstore.Category.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import pq.jdev.b001.bookstore.Category.model.Category;
-import pq.jdev.b001.bookstore.Category.web.CategoryWeb;
 
 
 @Repository
-public interface CategoryAddEditRepository extends JpaRepository<Category, Long>{
-	Category save(Category category);
+public interface CategoryRepository extends JpaRepository<Category, Long>, CrudRepository<Category, Long>{
+	
 }
