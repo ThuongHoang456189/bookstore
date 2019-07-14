ca47c62725a9a46e2fba7a831be76989bb08665e
ntnduy <ntnduy2905@gmail.com>
2019-07-14 17:12:53 +0700

    update publisherController

--- bookstore/src/main/java/pq/jdev/b001/bookstore/publishers/controller/PublisherController.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/publishers/controller/PublisherController.java
@@ -1,19 +1,24 @@
 package pq.jdev.b001.bookstore.publishers.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Flow.Publisher;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pq.jdev.b001.bookstore.publisher.models.Publishers;
 import pq.jdev.b001.bookstore.publishers.repository.PublisherRepository;
 import pq.jdev.b001.bookstore.publishers.service.PublisherService;
 import pq.jdev.b001.bookstore.publishers.service.PublisherServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class PublisherController {
@@ -50,4 +55,36 @@ public class PublisherController {
 		return "publishersList";
 	}
 
+	@GetMapping("/employee/page/{pageNumber}")
+	public String showEmployeePage(HttpServletRequest request, 
+			@PathVariable int pageNumber, Model model) {
+		PagedListHolder<?> pages = (PagedListHolder<?>) request.getSession().getAttribute("publiserList");
+		int pagesize = 3;
+		List<Publishers> list =(List<Publishers>) publisherService.findall();
+		System.out.println(list.size());
+		if (pages == null) {
+			pages = new PagedListHolder<>(list);
+			pages.setPageSize(pagesize);
+		} else {
+			final int goToPage = pageNumber - 1;
+			if (goToPage <= pages.getPageCount() && goToPage >= 0) {
+				pages.setPage(goToPage);
+			}
+		}
+		request.getSession().setAttribute("publiserList", pages);
+		int current = pages.getPage() + 1;
+		int begin = Math.max(1, current - list.size());
+		int end = Math.min(begin + 5, pages.getPageCount());
+		int totalPageCount = pages.getPageCount();
+		String baseUrl = "/employee/page/";
+
+		model.addAttribute("beginIndex", begin);
+		model.addAttribute("endIndex", end);
+		model.addAttribute("currentIndex", current);
+		model.addAttribute("totalPageCount", totalPageCount);
+		model.addAttribute("baseUrl", baseUrl);
+		model.addAttribute("employees", pages);
+
+		return "list";
+	}
 }
