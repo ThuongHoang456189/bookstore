5b07f651dac0dbd99b0f1dfc4f5348720b82143a
phuoc <phuocpro1969@google.com>
2019-07-23 16:49:26 +0700

    fix

--- bookstore/src/main/java/pq/jdev/b001/bookstore/users/config/WebSecurityConfig.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/users/config/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package pq.jdev.b001.bookstore.users.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import pq.jdev.b001.bookstore.users.service.UserService;
@@ -28,9 +24,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired
     CustomSuccessHandler successHandler;
-    
-    @Autowired
-    private DataSource dataSource;
     
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -51,8 +44,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .formLogin()
             .loginPage("/login").successHandler(successHandler).permitAll()
-            .and()
-            .rememberMe().rememberMeParameter("remember-me").tokenRepository(tokenRepository())
             .and().logout().invalidateHttpSession(true)
 			.clearAuthentication(true)
 			.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
@@ -60,13 +51,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
             .exceptionHandling()
             .accessDeniedPage("/403");
-    }
-    
-    @Bean
-    public PersistentTokenRepository tokenRepository() {
-      JdbcTokenRepositoryImpl jdbcTokenRepositoryImpl=new JdbcTokenRepositoryImpl();
-      jdbcTokenRepositoryImpl.setDataSource(dataSource);
-      return jdbcTokenRepositoryImpl;
     }
     
     @Bean
