0b26e48e5dc60420b3cbe76e1f7d984b524143c6
ntnduy <ntnduy2905@gmail.com>
2019-07-28 14:02:19 +0700

    update link publisher in homepage !

--- bookstore/src/main/java/pq/jdev/b001/bookstore/users/config/WebSecurityConfig.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/users/config/WebSecurityConfig.java
@@ -35,7 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/**","/registration**", "/forgot-password**", "/reset-password**").permitAll()
+            .antMatchers("/**","/registration**", "/forgot-password**", "/reset-password**","/publishersList**").permitAll()
             .antMatchers("/js/**", "/css/**", "/img/**", "/webjars/**","/user**").permitAll()
             .antMatchers("/accountUser").access("hasRole('EMPLOYEE')")
             .antMatchers("/user**").access("hasRole('EMPLOYEE') and hasRole('ADMIN')")
