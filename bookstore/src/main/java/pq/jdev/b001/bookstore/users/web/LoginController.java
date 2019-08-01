cbc30ec004a5bdf1eb4537ea3ae1388c4a7a5f6e
phuoc <phuocpro1969@google.com>
2019-08-01 09:43:40 +0700

    fix login

--- bookstore/src/main/java/pq/jdev/b001/bookstore/users/web/LoginController.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/users/web/LoginController.java
@@ -1,6 +1,12 @@
 package pq.jdev.b001.bookstore.users.web;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -10,48 +16,74 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-	
-	
-	@PreAuthorize("!(hasRole('EMPLOYEE') OR hasRole('ADMIN'))")
-	@GetMapping({"/"})
-	public String root() {
+
+	@GetMapping({ "/" })
+	public String root(Authentication authentication) {
+		if (authentication != null) {
+			Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+			List<String> roles = new ArrayList<String>();
+			for (GrantedAuthority a : authorities) {
+				roles.add(a.getAuthority());
+			}
+
+			if (isUser(roles)) {
+				return "/user";
+			} else if (isAdmin(roles)) {
+				return "/admin";
+			}
+		}
 		return "indexcontainer";
 	}
-	
+
 	@PreAuthorize("!(hasRole('EMPLOYEE') OR hasRole('ADMIN'))")
 	@GetMapping("/login")
 	public String login(Model model) {
 		return "login";
 	}
-	
+
 	@PreAuthorize("hasRole('EMPLOYEE')")
 	@GetMapping("/user")
 	public String userPage(ModelMap model) {
 		return "user";
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/admin")
 	public String getAdmin(ModelMap model) {
 		model.addAttribute("user", getPrincipal());
 		return "admin";
 	}
-	
+
 	@GetMapping(value = "/403")
-    public String accessDeniedPage(ModelMap model) {
-        model.addAttribute("user", getPrincipal());
-        return "403";
-    }
-	
-	private String getPrincipal(){
-        String userName = null;
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
- 
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails)principal).getUsername();
-        } else {
-            userName = principal.toString();
-        }
-        return userName;
-    }
+	public String accessDeniedPage(ModelMap model) {
+		model.addAttribute("user", getPrincipal());
+		return "403";
+	}
+
+	private String getPrincipal() {
+		String userName = null;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		if (principal instanceof UserDetails) {
+			userName = ((UserDetails) principal).getUsername();
+		} else {
+			userName = principal.toString();
+		}
+		return userName;
+	}
+
+	private boolean isUser(List<String> roles) {
+		if (roles.contains("ROLE_EMPLOYEE")) {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean isAdmin(List<String> roles) {
+		if (roles.contains("ROLE_ADMIN")) {
+			return true;
+		}
+		return false;
+	}
+
 }
