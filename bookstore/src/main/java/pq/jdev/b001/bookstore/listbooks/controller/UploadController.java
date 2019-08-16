f29ad1d97d92dd30afe6f41b4b0f98f6aa9d2299
Nguyễn Duy Hiếu <49218599+duyhieu789159@users.noreply.github.com>
2019-08-16 17:06:21 +0700

    fix

--- bookstore/src/main/java/pq/jdev/b001/bookstore/listbooks/controller/UploadController.java
+++ bookstore/src/main/java/pq/jdev/b001/bookstore/listbooks/controller/UploadController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
@@ -18,12 +19,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import pq.jdev.b001.bookstore.users.service.UserService;
+
 @Controller
 
 public class UploadController {
 
+	@Autowired
+	private UserService userService;
+
 	// Save the uploaded file to this folder
-	private static String UPLOADED_FOLDER = "C:/Users/laptop/Desktop/bookstore/src/main/resources/static/images/";
+	private static String UPLOADED_FOLDER = "C:/Users/duyhi/Desktop/booktest/bookstore/src/main/resources/static/images/";
 
 	@GetMapping("/upload")
 	public String viewUpLoad(Authentication authentication, ModelMap map) {
@@ -65,6 +71,51 @@ public class UploadController {
 
 		return "redirect:/";
 	}
+	
+	
+	
+	//Down images main
+	
+//	@PostMapping("/upload") // //new annotation since 4.3
+//	public String singleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, HttpServletResponse response) {
+//
+//		try {
+//
+//			
+//			String fullPath = UPLOADED_FOLDER + "/" + file.getOriginalFilename();
+//			File file2 = new File(fullPath);	
+//
+//			if (!file2.exists()) {
+//				String errorMessage = "Sorry. The file you are looking for does not exist";
+//				OutputStream outputStream = response.getOutputStream();
+//				outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
+//				outputStream.close();
+//				return "redirect:/upload";
+//			}
+//
+//			String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+//			if (mimeType == null) {
+//				System.out.println("mimetype is not detectable, will take default");
+//				mimeType = "application/octet-stream";
+//			}
+//
+//			response.setContentType(mimeType);
+//
+//			response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file2.getName() + "\""));
+//
+//			response.setContentLength((int) file2.length());
+//
+//			InputStream inputStream = new BufferedInputStream(new FileInputStream(file2));
+//
+//			FileCopyUtils.copy(inputStream, response.getOutputStream());
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		return "redirect:/";
+//	}
+
 
 	private boolean isUser(List<String> roles) {
 		if (roles.contains("ROLE_EMPLOYEE")) {
